1d5d406a115565268d854cf26c3fd0d7b1225b00
Felipe Cechin Mello <ficechin@hotmail.com>
2023-06-04 22:28:16 -0300

    react query mutation

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -29,13 +29,28 @@ export function PostDetail({ post }) {
     ["comments", post.id], () => fetchComments(post.id)
   )
 
+  const deleteMutation = useMutation((postId) => deletePost(postId))
+
   if (isLoading) return <h3>Loading...</h3>;
   if (isError) return <><h3>Something went wrong</h3><p>{error.toString()}</p></>;
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button
+        onClick={() => {
+          deleteMutation.mutate(post.id);
+        }}
+      >Delete</button> <button>Update title</button>
+      {
+        deleteMutation.isError && <p>Error deleting the post</p>
+      }
+      {
+        deleteMutation.isLoading && <p>Deleting the post</p>
+      }
+      {
+        deleteMutation.isSuccess && <p>Post deleted</p>
+      }
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
