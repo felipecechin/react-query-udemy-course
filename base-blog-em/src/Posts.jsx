581d37dd3046d19bb30dc8942f648c24c73745f8
Felipe Cechin Mello <ficechin@hotmail.com>
2023-06-04 22:18:26 -0300

    implementing pagination

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,26 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { PostDetail } from "./PostDetail";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+
+
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      queryClient.prefetchQuery(["posts", currentPage + 1], () => fetchPosts(currentPage + 1))
+    }
+  }, [currentPage, queryClient])
 
   // replace with useQuery
   const {data, isError, isLoading, error} = useQuery(
-    "posts",
-    fetchPosts,
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
     {
-      staleTime: 2000
+      staleTime: 2000,
+      keepPreviousData: true,
     }
   )
 
@@ -41,11 +51,15 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <= 1} onClick={() => {
+          setCurrentPage(previousPage => previousPage - 1)
+        }}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage >= maxPostPage} onClick={() => {
+          setCurrentPage(previousPage => previousPage + 1)
+        }}>
           Next page
         </button>
       </div>
