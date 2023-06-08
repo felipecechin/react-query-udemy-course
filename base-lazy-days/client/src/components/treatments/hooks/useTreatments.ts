3c3f87834f74a6771c5bf0dfed86d68a8111fb5f
Felipe Cechin Mello <ficechin@hotmail.com>
2023-06-08 01:22:59 -0300

    using react-query hooks

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,9 +1,8 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -16,4 +15,9 @@ export function useTreatments(): Treatment[] {
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
 
   return data;
+}
+
+export function usePrefetchTreatments(): void {
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,17 +1,29 @@
-import { QueryClient } from 'react-query';
+import { createStandaloneToast } from '@chakra-ui/react';
+import { QueryCache, QueryClient } from 'react-query';
 
-// import { createStandaloneToast } from '@chakra-ui/react';
-// import { theme } from '../theme';
+import { theme } from '../theme';
 
-// const toast = createStandaloneToast({ theme });
+const toast = createStandaloneToast({ theme });
 
-// function queryErrorHandler(error: unknown): void {
-//   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-//   const title =
-//     error instanceof Error ? error.message : 'error connecting to server';
+function queryErrorHandler(error: unknown): void {
+  // error is type unknown because in js, anything can be an error (e.g. throw(5))
+  const title =
+    error instanceof Error ? error.message : 'error connecting to server';
 
-//   // prevent duplicate toasts
-//   toast.closeAll();
-//   toast({ title, status: 'error', variant: 'subtle', isClosable: true });
-// }
-export const queryClient = new QueryClient();
+  /// ////////////////////////////
+  // NOTE: no toast.closeAll() //
+  /// ////////////////////////////
+  toast.closeAll();
+  toast({ title, status: 'error', variant: 'subtle', isClosable: true });
+}
+
+export const queryClient = new QueryClient({
+  // queryCache: new QueryCache({
+  //   onError: queryErrorHandler,
+  // }),
+  defaultOptions: {
+    queries: {
+      onError: queryErrorHandler,
+    },
+  },
+});
